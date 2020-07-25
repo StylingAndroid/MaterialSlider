90a9b4826e8b8e24c2376b28f134efacface0158
Mark Allison <mark@stylingandroid.com>
2020-07-25 14:46:43 +0100

    Code for Slider article

---
+++ app/src/main/java/com/stylingandroid/material/slider/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.stylingandroid.material.slider
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.stylingandroid.material.slider.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.discreteRange.setLabelFormatter {
+            getString(R.string.label_format, it)
+        }
+    }
+}
